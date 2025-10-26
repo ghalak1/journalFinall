fe4fdb5d4f0acd017fad3d8658926d26a8838e02
g <ghalakns5@gmail.com>
2025-10-26 04:10:09 -0700

    Initial Commit

---
+++ journal1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  journal1
+//
+//  Created by ghala alismael on 04/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ journal1/journal1App.swift
@@ -0,0 +1,17 @@
+//
+//  journal1App.swift
+//  journal1
+//
+//  Created by ghala alismael on 04/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct journal1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
